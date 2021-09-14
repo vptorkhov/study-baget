affe723456c348f8aee6e0df264ee2c069e6d075
vptorkhov <vptorkhov@gmail.com>
2021-09-15 00:28:40 +0300

    sliders and pics

---
+++ scripts/common.js
@@ -0,0 +1,56 @@
+var swiper = new Swiper(".mainSwiper", {
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
+var swiper = new Swiper(".examplesSwiper", {
+	slidesPerView: 2,
+	spaceBetween: 0,
+	slidesPerGroup: 3,
+	loop: true,
+	loopFillGroupWithBlank: true,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+	breakpoints: {
+		992: {
+			spaceBetween: 20,
+			slidesPerView: 2
+		},
+		1400: {
+			spaceBetween: 30,
+			slidesPerView: 3
+		}
+	}
+});
+var swiper = new Swiper(".specSwiper", {
+	slidesPerView: 2,
+	spaceBetween: 0,
+	slidesPerGroup: 3,
+	loop: true,
+	loopFillGroupWithBlank: true,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+	breakpoints: {
+		992: {
+			spaceBetween: 20,
+			slidesPerView: 2
+		},
+		1400: {
+			spaceBetween: 30,
+			slidesPerView: 3
+		}
+	}
+});
